44ec2cda34d3e75546ca4361f92e167002dda8f4
Sharmi-saga <sharmitha2332@gmail.com>
2024-09-11 13:58:09 +0530

    Feat : Creating an Appointment Interface

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AppointmentListComponent  {
+   
+  appointment : Appointment = {
+    id : 1,
+    title : " First Appointment ",
+    date : new Date('2024-07-30')
   }
+
 
 }
