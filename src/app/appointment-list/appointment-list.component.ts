0f3fdc64f3a24ba90b177a0b696ca469959ee937
Sharmi-saga <sharmitha2332@gmail.com>
2024-09-12 13:26:55 +0530

    Fix: Add and Delete Btn

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -6,15 +6,41 @@ import { Appointment } from '../models/appointment';
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent  {
+export class AppointmentListComponent implements OnInit {
+
+  ngOnInit(): void {
+    let savedAppointment = localStorage.getItem("appointments")9
+  console.log("got log")
+
+  }
    
   newAppointmentTittle : string ="";
   newAppointmentDate : Date = new Date; 
-  appointments : Appointment[] =[]
+  appointments : Appointment[] =[];
 
   addAppointment()
   {
-     alert( "Your Appointment Name and date :-" + this.newAppointmentTittle + " " + this.newAppointmentDate )
+    // alert( "Your Appointment Name and date :-" + this.newAppointmentTittle + " " + this.newAppointmentDate )
+    if(this.newAppointmentTittle.trim().length && this.newAppointmentDate)
+    {
+      let newAppointment: Appointment ={
+        id: Date.now(),
+        title: this.newAppointmentTittle,
+        date: this.newAppointmentDate
+      }
+
+      this.appointments.push(newAppointment)
+      this.newAppointmentTittle = "";
+      this.newAppointmentDate = new Date();
+      //alert(this.appointments.length)
+      localStorage.setItem("appointments" , JSON.stringify(this.appointments))
+    }
+  }
+
+  deleteAppointment(index :number)
+  {
+    this.appointments.splice(index ,1)
+    localStorage.setItem("appointments" , JSON.stringify(this.appointments))  
   }
 
 }
